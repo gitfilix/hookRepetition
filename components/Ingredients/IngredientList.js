13d4ebcc5841d6e9cbdc984aada464c32b0ca2c9
gitfilix <runlevel07@gmail.com>
2021-05-24 13:29:08 +0200

    share state over multiple components

--- components/Ingredients/IngredientList.js
+++ components/Ingredients/IngredientList.js
@@ -5,7 +5,7 @@ import './IngredientList.css';
 const IngredientList = props => {
   return (
     <section className="ingredient-list">
-      <h2>Loaded Ingredients</h2>
+      <h2>Loaded Ingredients (Ingredient-list)</h2>
       <ul>
         {props.ingredients.map(ig => (
           <li key={ig.id} onClick={props.onRemoveItem.bind(this, ig.id)}>
