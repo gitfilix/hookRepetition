658eaa037b47a740e41cd0a973be0c9ee4ec4d6f
gitfilix <runlevel07@gmail.com>
2021-05-25 22:07:02 +0200

    add another reducer for all http actions

--- components/Ingredients/Ingredients.js
+++ components/Ingredients/Ingredients.js
@@ -1,4 +1,4 @@
-import React, {useState, useEffect, useCallback, useReducer } from 'react';
+import React, { useEffect, useCallback, useReducer } from 'react';
 
 import IngredientForm from './IngredientForm';
 import IngredientList from './IngredientList'
@@ -6,44 +6,40 @@ import ErrorModal from '../UI/ErrorModal'
 import Search from './Search';
 
 const ingReducer = (currentIngredients, action) => {
-    switch(action.type) {
-      case 'SET':
-        return action.ingredients
-      case 'ADD':
-        return [...currentIngredients, action.ingredient]
-      case 'DELETE':
-        return currentIngredients.filter(ing => ing.id !== action.id)
-      default:
-        throw new Error('Reducer Error: Should not get there! no case match ')
-    }
+  switch(action.type) {
+    case 'SET':
+      return action.ingredients
+    case 'ADD':
+      return [...currentIngredients, action.ingredient]
+    case 'DELETE':
+      return currentIngredients.filter(ing => ing.id !== action.id)
+    default:
+      throw new Error('Reducer Error: Should not get there! no case match ')
+  }
+}
+
+const httpReducer = (prevHttpState, action) => {
+  switch(action.type) {
+    case 'SEND':
+      return { loading: true, error: null }
+    case 'RESPONSE':
+      return { ...prevHttpState, loading: false }
+    case 'ERROR':
+      return { loading: false, error: action.errorMessage }
+    case 'CLEAR':
+      return { ...prevHttpState, error: null }
+      default: throw new Error('httpReducer unknown Error - should not be reached.')
+  }
 }
 
 const Ingredients = ()=> {
   // reducer init: state (here userIngredients, dispatch) 
   // useReducer: reducer function, initial state: here []
   const [ userIngredients, dispatch ] = useReducer(ingReducer, [])
-  // const [ userIngredients, setUserIngredients ] = useState([])
-  const [ isLoading, setIsLoading ] = useState(false)
-  const [ error, setIsError ] = useState()
+  const [ httpState, dispatchHttp] = useReducer(httpReducer, { loading: false, error: null})
+  // const [ isLoading, setIsLoading ] = useState(false)
+  // const [ error, setIsError ] = useState()
 
-  /*
-  useEffect(() => {
-    // get Data
-    fetch('https://hookrepetition-default-rtdb.europe-west1.firebasedatabase.app/ingredients.json')
-    .then(response => response.json())
-    .then(responseData => {
-      const loadedIngredients = []
-      for (const key in responseData) {
-        loadedIngredients.push({
-          id: key,
-          title: responseData[key].title,
-          amount: responseData[key].amount
-        })
-      }
-      setUserIngredients(loadedIngredients)
-    })
-  }, [])
-*/
   // for search component
   const filteredIngredientsHandler = useCallback(fileredIngredients => {
     // setUserIngredients(fileredIngredients)
@@ -52,14 +48,17 @@ const Ingredients = ()=> {
 
   // handler called from form to update ingreds and pass it to the list
   const addIngHandler = (ingredient) => {
-    setIsLoading(true)
+    // useHTTP reducer here:
+    dispatchHttp({type: 'SEND'})
+    // setIsLoading(true)
     // use fetch for a POST message to POST data into Firebase
     fetch('https://hookrepetition-default-rtdb.europe-west1.firebasedatabase.app/ingredients.json', {
       method: 'POST',
       body: JSON.stringify(ingredient),
       headers: { 'Content-Type': 'application/json' }
     }).then(response => {
-      setIsLoading(false)
+      dispatchHttp({ type: 'RESPONSE' })
+      // setIsLoading(false)
       return response.json()
     }).then(responseData => {
       // setUserIngredients(prevIngredients => [
@@ -71,33 +70,37 @@ const Ingredients = ()=> {
   }
 
   const removeIngHandler = (ingredientId) => {
-    setIsLoading(true)
+    dispatchHttp({type: 'SEND'})
+    // setIsLoading(true)
     fetch(`https://hookrepetition-default-rtdb.europe-west1.firebasedatabase.app/ingredients/${ingredientId}.json`, {
       method: 'DELETE',
     })
     .then(response => {
-      setIsLoading(false)
+      dispatchHttp({type: 'RESPONSE'})
+      // setIsLoading(false)
       // setUserIngredients(prevIngredients => 
         // prevIngredients.filter(ingredient => ingredient.id !== ingredientId)
       // )
       dispatch({ type: 'DELETE', id: ingredientId })
       
     }).catch(error => {
-      setIsError('something went wrong')
-      setIsLoading(false)
+      dispatchHttp({ type: 'ERROR', errorMessage: 'Something when wrong here' })
+      // setIsError('something went wrong')
+      // setIsLoading(false)
     }) 
   }
 
   const clearError = () => {
-    setIsError(null)
+    dispatchHttp({ type: 'CLEAR' })
+    // setIsError(null)
   }
 
   return (
     <div className="App">
-      {error && <ErrorModal onClose={clearError}>{error}</ErrorModal>}
+      {httpState.error && <ErrorModal onClose={clearError}>{httpState.error}</ErrorModal>}
       <IngredientForm 
         onAddIngredient={addIngHandler} 
-        loading={isLoading} />
+        loading={httpState.loading} />
 
       <section>
         <Search onLoadIngredients={filteredIngredientsHandler} />
